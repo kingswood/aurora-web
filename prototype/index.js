a6e55dfdf8688a3495b55d1aa056836e6e3b9cd5
Raymond Liu <kingswood0912@gmail.com>
2018-07-10 16:10:11 +0800

    Adding prototype pages built on bootstrap.
    Made by Nancy

---
+++ prototype/index.js
@@ -0,0 +1,8 @@
+﻿var myApp = angular.module('app',[]);
+
+myApp.controller('NavCtrl', function ($scope) {
+    $scope.setParmeter = function (parameter) {
+        sessionStorage.setItem("item", parameter);
+    };
+    
+})
